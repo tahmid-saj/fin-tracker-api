0ab8239458b50437c97da0eda6ea66d8dec1f958
tahmid-saj <tahmid.saj@gmail.com>
2023-10-22 23:31:49 -0400

    routes for banking

--- src/routes/banking/banking.controller.js
+++ src/routes/banking/banking.controller.js
@@ -1,9 +1,41 @@
-const { getBankingData } = require("../../models/banking/banking.model")
+const { getBankingAccountsData, getBankingSummaryData,
+  postBankingAccountCreate, postBankingAccountTransaction, deleteBankingAccount,
+  putBankingAccountsData, putBankingSummaryData } = require("../../models/banking/banking.model")
 
-async function httpGetBankingData(req, res) {
-  return res.status(200).json(await getBankingData());
+async function httpGetBankingAccountsData(req, res) {
+  return res.status(200).json(await getBankingAccountsData());
 };
 
+async function httpGetBankingSummaryData(req, res) {
+  return res.status(200).json(await getBankingSummaryData());
+};
+
+async function httpPostBankingAccountCreate(req, res) {
+  return res.status(200).json(await postBankingAccountCreate()); 
+};
+
+async function httpPostBankingAccountTransaction(req, res) {
+  return res.status(200).json(await postBankingAccountTransaction());
+};
+
+async function httpDeleteBankingAccount(req, res) {
+  return res.status(200).json(await deleteBankingAccount());
+};
+
+async function httpPutBankingAccountsData(req, res) {
+  return res.status(200).json(await putBankingAccountsData());
+};
+
+async function httpPutBankingSummaryData(req, res) {
+  return res.status(200).json(await putBankingSummaryData());
+}
+
 module.exports = {
-  httpGetBankingData,
+  httpGetBankingAccountsData,
+  httpGetBankingSummaryData,
+  httpPostBankingAccountCreate,
+  httpPostBankingAccountTransaction,
+  httpDeleteBankingAccount,
+  httpPutBankingAccountsData,
+  httpPutBankingSummaryData
 }
