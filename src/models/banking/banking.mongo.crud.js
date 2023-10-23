dfdf74d4d730b6a5472771a29c5d101c541d9209
tahmid-saj <tahmid.saj@gmail.com>
2023-10-23 14:12:30 -0400

    banking account creation for mongo

--- src/models/banking/banking.mongo.crud.js
+++ src/models/banking/banking.mongo.crud.js
@@ -1,6 +1,27 @@
-import { bankingAccountsDatabase, bankingSummaryDatabase } from "./banking.mongo";
+const { bankingAccountsDatabase, bankingSummaryDatabase } = require("./banking.mongo");
 
 // banking crud for mongodb
+
+async function createBankingSummary(userId, email) {
+  const bankingSummaryExists = await bankingSummaryDatabase.findOne({
+    userId: userId,
+    email: email
+  });
+
+  if (!bankingSummaryExists) {
+    const newBankingSummary = new bankingSummaryDatabase({
+      userId: userId,
+      email: email,
+      currentAllBankingBalance: 0,
+      totalAllBankingIn: 0,
+      totalAllBankingOut: 0,
+    });
+  
+    await newBankingSummary.save();
+  } else {
+    return;
+  }
+};
 
 async function createBankingAccount(userId, email, bankingAccountName) {
   const newBankingAccount = new bankingAccountsDatabase({
@@ -14,8 +35,12 @@ async function createBankingAccount(userId, email, bankingAccountName) {
   });
 
   await newBankingAccount.save();  
+
+  // if this is the first bank account, a new banking summary will be created
+  await createBankingSummary(userId, email);
 };
 
 module.exports = {
   createBankingAccount,
+  createBankingSummary
 }
