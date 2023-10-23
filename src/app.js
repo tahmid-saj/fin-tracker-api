60f0a7da681599588e2bef091c8d0d5a4bc6a0fb
tahmid-saj <tahmid.saj@gmail.com>
2023-10-22 21:54:17 -0400

    server and api

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const api = require("./routes/api.routes");
+
+const app = express();
+
+app.use(cors({
+  origin: "https://localhost:3000",
+}));
+app.use(morgan("combined"));
+app.use(express.json());
+app.use('/v1', api);
+
+module.exports = {
+  app,
+};
