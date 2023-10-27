537aac2aa03e30598c5c5ca5a58bb0046ab783f6
tahmid-saj <tahmid.saj@gmail.com>
2023-10-26 22:54:42 -0400

    savings mongo db

---
+++ src/models/savings/savings.mongo.js
@@ -0,0 +1,94 @@
+const mongoose = require('mongoose');
+
+const savingsAccountsSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  savingsAccountName: {
+    type: String,
+    required: true,
+  },
+  initialDeposit: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  monthlyContribution: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  contributionPeriod: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  contributionInterval: {
+    type: String,
+    required: true,
+  },
+  apy: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  // calculated
+  totalSavings: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  totalContribution: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  totalInterest: {
+    type: Number,
+    required: true,
+    default: 0,
+  }
+});
+
+const savingsAccountsSummarySchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  currentAllSavingsAccountsBalance: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  totalAllContribution: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  totalAllInterest: {
+    type: Number,
+    required: true,
+    default: 0,
+  }
+});
+
+const savingsAccountsDatabase = mongoose.model("SavingsAccounts", savingsAccountsSchema);
+const savingsAccountsSummaryDatabase = mongoose.model("SavingsAccountsSummary", savingsAccountsSummarySchema);
+
+module.exports = {
+  savingsAccountsDatabase,
+  savingsAccountsSummaryDatabase
+};
