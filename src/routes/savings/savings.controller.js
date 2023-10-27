69bc590813dacd93dd0aba9b81a2edb738c03e94
tahmid-saj <tahmid.saj@gmail.com>
2023-10-26 22:33:24 -0400

    savings router

---
+++ src/routes/savings/savings.controller.js
@@ -0,0 +1,10 @@
+const { getSavingsAccountsData, getSavingsAccountsSummaryData, 
+  postSavingsAccountCreate, putSavingsAccountData, deleteSavingsAccount,
+  putSavingsAccountsData, putSavingsAccountsSummaryData } = require("../../models/savings/savings.model");
+
+// signed in
+
+
+// savings operations
+
+// signed out
---
+++ src/routes/savings/savings.router.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const { httpGetSavingsAccountsData, httpGetSavingsAccountsSummaryData,
+  httpPostSavingsAccountCreate, httpPutSavingsAccountData, httpDeleteSavingsAccount,
+  httpPutSavingsAccountsData, httpPutSavingsAccountsSummaryData } = require("./savings.controller");
+
+const savingsRouter = express.router();
+
+// TODO: move to env variables
+// when user is signed in
+savingsRouter.get("/accounts/:userid/:email", httpGetSavingsAccountsData);
+savingsRouter.get("/summary/:userid/:email", httpGetSavingsAccountsSummaryData);
+
+// investments operations
+savingsRouter.post("/accounts/:userid/:email/create", httpPostSavingsAccountCreate);
+savingsRouter.put("/accounts/:userid/:email/update", httpPutSavingsAccountData);
+savingsRouter.delete("/accounts/:userid/:email/close", httpDeleteSavingsAccount);
+
+// user is signing out
+savingsRouter.put("/accounts/:userid/:email", httpPutSavingsAccountsData);
+savingsRouter.put("/summary/:userid/:email", httpPutSavingsAccountsSummaryData);
+
+module.exports = {
+  savingsRouter,
+}
