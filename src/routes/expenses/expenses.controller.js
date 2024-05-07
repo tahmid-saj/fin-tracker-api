764c520437ccbc79853d360f247535697b73fdf0
tahmid-saj <tahmid.saj@gmail.com>
2024-05-06 23:54:43 -0400

    expenses router

---
+++ src/routes/expenses/expenses.controller.js
@@ -0,0 +1,35 @@
+// signed in
+async function httpGetExpensesData(req, res) {
+
+}
+
+async function httpGetExpensesSummaryData(req, res) {
+
+}
+
+// expenses operations
+async function httpPostExpenseCreate(req, res) {
+
+}
+
+async function httpDeleteExpense(req, res) {
+
+}
+
+// signed out
+async function httpPutExpensesData(req, res) {
+
+}
+
+async function httpPutExpensesSummaryData(req, res) {
+
+}
+
+module.exports = {
+  httpGetExpensesData,
+  httpGetExpensesSummaryData,
+  httpPostExpenseCreate,
+  httpDeleteExpense,
+  httpPutExpensesData,
+  httpPutExpensesSummaryData,
+}
