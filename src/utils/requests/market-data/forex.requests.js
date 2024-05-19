4d8837e97d4b74275a6f078c09f6e135dc15d166
tahmid-saj <tahmid.saj@gmail.com>
2024-05-19 17:42:08 -0400

    vercel updates

---
+++ src/utils/requests/market-data/forex.requests.js
@@ -0,0 +1,11 @@
+const { restClient } = require("@polygon.io/client-js")
+
+const polygonRestClient = restClient("")
+
+// polygonRestClient.forex.aggregates("C:USDCAD", 1, "week", "2024-01-01", "2024-02-02")
+// .then(res => console.log(res))
+// .catch(err => console.log(err))
+
+polygonRestClient.forex.previousClose("C:CADUSD")
+.then(res => console.log(res))
+.catch(err => console.log(err))
---
+++ src/utils/requests/market-data/stocks.requests.js
@@ -0,0 +1,77 @@
+const { restClient } = require("@polygon.io/client-js")
+
+const polygonRestClient = restClient("")
+const { toDate } = require("unix-timestamp")
+
+// polygonRestClient.stocks.aggregates("AAPL", 1, "day", "2024-01-01", "2024-02-02")
+// .then(res => console.log(res))
+// .catch(err => console.log(err))
+
+// helper functions
+
+function convUnix(unix_timestamp) {
+  // Create a new JavaScript Date object based on the timestamp
+  // multiplied by 1000 so that the argument is in milliseconds, not seconds
+  var date = new Date(unix_timestamp);
+  
+  // Hours part from the timestamp
+  var hours = date.getHours();
+  
+  // Minutes part from the timestamp
+  var minutes = "0" + date.getMinutes();
+  
+  // Seconds part from the timestamp
+  var seconds = "0" + date.getSeconds();
+  
+  // Will display time in 10:30:23 format
+  var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  
+  return {
+    date: date,
+    time: formattedTime
+  }
+}
+
+async function processMarketDataResponse(marketDataRes) {
+  // console.log(marketDataRes.results)
+  return marketDataRes.results.map((marketDataRecord) => {
+    return {
+      closing: marketDataRecord.c,
+      time: convUnix(marketDataRecord.t)
+    }
+  })
+}
+
+// stocks
+async function getStocksMarketData(marketDataQuery) {
+  const res = polygonRestClient.stocks.aggregates(
+    marketDataQuery.marketDataTicker, 
+    1, 
+    marketDataQuery.marketDataInterval,
+    marketDataQuery.marketDataStartDate,
+    marketDataQuery.marketDataEndDate
+    ).then(async (res) => {
+      const marketData = await processMarketDataResponse(res)
+      // console.log(typeof(marketData))
+      return marketData
+    }).catch((error) => {
+      // errorOnGetStocksMarketData()
+      console.log(error)
+      return Object({})
+    })
+  
+  return res
+}
+
+async function getRes() { 
+  const res = await getStocksMarketData({
+    marketDataType: "stocks",
+    marketDataTicker: "AAPL",
+    marketDataInterval: "hour",
+    marketDataStartDate: "2024-01-01",
+    marketDataEndDate: "2024-01-03",
+  })
+  console.log(res)
+}
+
+getRes()
