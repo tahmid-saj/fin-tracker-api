2b53284b831be7cba64992f5e7b06e7dea7efef7
tahmid-saj <tahmid.saj@gmail.com>
2024-06-13 00:15:42 -0400

    removing method names

--- src/routes/banking/banking.router.js
+++ src/routes/banking/banking.router.js
@@ -13,11 +13,11 @@ bankingRouter.get("/accounts/:userid/:email", httpGetBankingAccountsData);
 bankingRouter.get("/summary/:userid/:email", httpGetBankingSummaryData);
 
 // when user is creating a new bank account, frontend will send this and add the bank account data to mongodb
-bankingRouter.post("/accounts/:userid/:email/create", httpPostBankingAccountCreate);
+bankingRouter.post("/accounts/:userid/:email", httpPostBankingAccountCreate);
 // when user is creating a transaction, frontend will send this and add the transaction data to mongodb
 bankingRouter.post("/accounts/:userid/:email/transaction", httpPostBankingAccountTransaction);
 // when user is closing a bank account, frontend will send this and delete the bank account from mongodb
-bankingRouter.delete("/accounts/:userid/:email/close", httpDeleteBankingAccount);
+bankingRouter.delete("/accounts/:userid/:email", httpDeleteBankingAccount);
 
 // when user is signed out, frontend will send this request and the updated banking accounts data to mongodb
 bankingRouter.put("/accounts/:userid/:email", httpPutBankingAccountsData);
--- src/routes/savings/savings.router.js
+++ src/routes/savings/savings.router.js
@@ -12,9 +12,9 @@ savingsRouter.get("/accounts/:userid/:email", httpGetSavingsAccountsData);
 savingsRouter.get("/summary/:userid/:email", httpGetSavingsAccountsSummaryData);
 
 // investments operations
-savingsRouter.post("/accounts/:userid/:email/create", httpPostSavingsAccountCreate);
-savingsRouter.put("/accounts/:userid/:email/update", httpPutSavingsAccountData);
-savingsRouter.delete("/accounts/:userid/:email/close", httpDeleteSavingsAccount);
+savingsRouter.post("/accounts/:userid/:email", httpPostSavingsAccountCreate);
+savingsRouter.put("/accounts/:userid/:email", httpPutSavingsAccountData);
+savingsRouter.delete("/accounts/:userid/:email", httpDeleteSavingsAccount);
 
 // user is signing out
 savingsRouter.put("/accounts/:userid/:email", httpPutSavingsAccountsData);
