926cf30d57a29286d1b6919b0a261e6974235bff
tahmid-saj <tahmid.saj@gmail.com>
2024-08-03 22:35:17 -0400

    graphql investments and summary

--- src/graphql/investments/investments.model.js
+++ src/graphql/investments/investments.model.js
@@ -1,5 +1,6 @@
 const { getInvestments, getInvestmentsSummary,
-  createInvestment
+  createInvestment, updateInvestment, closeInvestment,
+  updateInvestments
 } = require("../../models/investments/investments.mongo.crud")
 
 async function getInvestmentsByUser(userId, email) {
@@ -18,8 +19,29 @@ async function createUserInvestment(userId, email, investmentInfo) {
   return true
 }
 
+async function updateUserInvestment(userId, email, originalInvestmentInfo, updatedInvestmentInfo) {
+  console.log("Updating investment");
+  updateInvestment(userId, email, originalInvestmentInfo, updatedInvestmentInfo);
+  return true
+}
+
+async function deleteUserInvestment(userId, email, closingInvestmentName) {
+  closeInvestment(userId, email, closingInvestmentName);
+  console.log("Deleting investment");
+  return true
+}
+
+async function updateUserInvestments(userId, email, investments) {
+  updateInvestments(userId, email, investments);
+  console.log("Putting investments data");
+  return true
+}
+
 module.exports = {
   getInvestmentsByUser,
   getInvestmentsSummaryByUser,
-  createUserInvestment
+  createUserInvestment,
+  updateUserInvestment,
+  deleteUserInvestment,
+  updateUserInvestments
 }
--- src/graphql/investments/investments.resolvers.js
+++ src/graphql/investments/investments.resolvers.js
@@ -12,6 +12,20 @@ module.exports = {
   Mutation: {
     createUserInvestment: (parent, args) => {
       return investmentsModel.createUserInvestment(args.userId, args.email, args.investmentInfo)
+    },
+    updateUserInvestment: (parent, args) => {
+      const originalInvestmentInfo = args.investmentInfoUpdate.originalInvestmentInfo
+      const updatedInvestmentInfo = args.investmentInfoUpdate.updatedInvestmentInfo
+      return investmentsModel.updateUserInvestment(args.userId, args.email, originalInvestmentInfo, updatedInvestmentInfo)
+    },
+    deleteUserInvestment: (parent, args) => {
+      return investmentsModel.deleteUserInvestment(args.userId, args.email, args.closingInvestmentName)
+    },
+    updateUserInvestments: (parent, args) => {
+      return investmentsModel.updateUserInvestments(args.userId, args.email, args.investments)
+    },
+    updateUserInvestmentsSummary: (parent, args) => {
+      return investmentsModel.updateUserInvestmentsSummary(args.userId, args.email, args.investmentsSummary)
     }
   }
 }
