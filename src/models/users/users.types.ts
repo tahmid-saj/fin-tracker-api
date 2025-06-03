9b88d8328e805e1fbd51938b2700d17527a1440e
tahmid-saj <tahmid.saj@gmail.com>
2025-06-02 20:34:44 -0400

    redis - storing sessions

--- src/models/users/users.types.ts
+++ src/models/users/users.types.ts
@@ -3,3 +3,9 @@ export type User = {
   userId: string,
   email: string
 }
+
+export type Session = {
+  sessionId: string,
+  userId: string,
+  email: string
+}
---
+++ src/redis/queries/sessions/sessions.queries.ts
@@ -0,0 +1,33 @@
+import { Session } from "../../../models/users/users.types";
+import { redisClient } from "../../../services/redis/redis.service";
+import { sessionsKey } from "./sessions.keys";
+
+// helper functions
+const serialize = (session: Session) => {
+  return {
+    userId: session.userId,
+    email: session.email
+  }
+}
+
+const deserialize = (sessionId: string, session: { [key: string]: string }) => {
+  return {
+    sessionId,
+    userId: session.userId,
+    email: session.email
+  }
+}
+
+export const getSession = async (sessionId: string) => {
+  const session = await redisClient.hGetAll(sessionsKey(sessionId))
+
+  if (Object.keys(session).length === 0) {
+    return null
+  }
+
+  return deserialize(session.sessionId!, session)
+}
+
+export const saveSession = async (session: Session) => {
+  return redisClient.hSet(sessionsKey(session.sessionId), serialize(session))
+}
