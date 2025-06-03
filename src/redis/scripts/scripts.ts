7c5fc703832f39da534f5f1e5089d40954046a4a
tahmid-saj <tahmid.saj@gmail.com>
2025-06-03 03:10:26 -0400

    lua scripts

--- src/redis/scripts/scripts.ts
+++ src/redis/scripts/scripts.ts
@@ -1,4 +1,84 @@
 
-export const luaScripts = {
-  
-}
+export const incrementMarketDataRequest = `
+  local marketDataUniqueRequestsKey = KEYS[1]
+  local usersKey = KEYS[2]
+  local marketDataRequestsKey = KEYS[3]
+  local marketDataRequestsByPopularityKey = KEYS[4]
+
+  local marketDataType = ARGV[1]
+  local marketDataTicker = ARGV[2]
+  local marketDataInterval = ARGV[3]
+  local marketDataStartDate = ARGV[4]
+  local marketDataEndDate = ARGV[5]
+  local userAuthenticated = ARGV[6]
+
+  if userAuthenticated == 1 then
+    local inserted = redis.call('PFADD', marketDataUniqueRequestsKey, usersKey)
+    if inserted == 1 then
+      redis.call('HINCRBY', marketDataRequestsKey, 'requests', 1)
+      redis.call('ZINCRBY', 'marketDataRequestsByPopularityKey', 1, marketDataRequestsKey)
+    end
+  end
+`
+
+export const updateLivePricesRequestTime = `
+  local livePricesUniqueRequestsKey = KEYS[1]
+  local usersKey = KEYS[2]
+  local livePricesRequestsKey = KEYS[3]
+  local livePricesRecentRequestsKey = KEYS[4]
+
+  local marketDataType = ARGV[1]
+  local marketDataTicker = ARGV[2]
+  local marketDataInterval = ARGV[3]
+  local marketDataStartDate = ARGV[4]
+  local marketDataEndDate = ARGV[5]
+  local userAuthenticated = ARGV[6]
+  local timeNow = ARGV[7]
+
+  if userAuthenticated then
+    local inserted = redis.call('PFADD', livePricesUniqueRequestsKey, usersKey)
+    if inserted then
+      redis.call('HINCRBY', livePricesRequestsKey, 'requestTime', timeNow)
+      redis.call('RPUSH', livePricesRecentRequestsKey, livePricesRequestsKey)
+    end
+  end
+
+  local recentPricesCount = redis.call('LLEN', livePricesRecentRequestsKey)
+  if recentPricesCount > 20 then
+    redis.call('LPOP', livePricesRecentRequestsKey)
+  end
+`
+
+export const updateLivePricesPopularTickers = `
+  local livePricesPopularTickersKey = KEYS[1]
+  local livePricesUniquePopularTickersKey = KEYS[2]
+  local usersKey = KEYS[3]
+
+  local marketDataType = ARGV[1]
+  local marketDataTicker = ARGV[2]
+  local marketDataInterval = ARGV[3]
+  local marketDataStartDate = ARGV[4]
+  local marketDataEndDate = ARGV[5]
+  local userAuthenticated = ARGV[6]
+
+  local tickerIsMember = redis.call('ZSCORE', livePricesPopularTickersKey, marketDataTicker)
+
+  if tickerIsMember == 0 then
+    redis.call('ZADD', livePricesPopularTickersKey, marketDataTicker, 1)
+  else
+    if userAuthenticated then
+      local inserted = redis.call('PFADD', livePricesUniquePopularTickersKey, usersKey)
+
+      if inserted then
+        redis.call('ZINCRBY', livePricesPopularTickersKey, 1, marketDataTicker)
+      end
+    else
+      redis.call('ZINCRBY', livePricesPopularTickersKey, 1, marketDataTicker)
+    end
+  end
+
+  local livePricesPopularTickersCount = redis.call('ZCARD', livePricesPopularTickersKey)
+  if livePricesPopularTickersCount > 10 then
+    redis.call('ZPOPMIN', livePricesPopularTickersKey)
+  end
+`
--- src/services/redis/redis.service.ts
+++ src/services/redis/redis.service.ts
@@ -1,4 +1,10 @@
 import { createClient } from "redis"
+import { incrementMarketDataRequest } from "../../redis/scripts/scripts.js"
+import { User } from "../../models/users/users.types.js"
+import { MarketDataRequest } from "../../models/market-data/market-data.types.js"
+import { marketDataRequestsKey, marketDataUniqueRequestsKey, 
+  marketDataRequestsByPopularityKey } from "../../redis/queries/market-data/market-data.keys.js"
+import { usersKey } from "../../redis/queries/users/users.keys.js"
 
 export const redisClient = createClient({
   socket: {
@@ -7,6 +13,27 @@ export const redisClient = createClient({
   },
   password: process.env.REDIS_PW
 })
+
+// export const incrementMarketDataRequestScript = redisClient.extendWithScripts({
+//   NUMBER_OF_KEYS: 4,
+//   SCRIPT: incrementMarketDataRequest,
+//   transformArguments(marketDataRequest: MarketDataRequest, user?: User) {
+//     if (user) {
+//       return [
+//         marketDataUniqueRequestsKey(), usersKey(user), marketDataRequestsKey(marketDataRequest), marketDataRequestsByPopularityKey(),
+//         marketDataRequest.marketDataType, marketDataRequest, marketDataRequest, marketDataRequest, marketDataRequest,
+//         1
+//       ]
+//     }
+
+//     return [
+//       marketDataUniqueRequestsKey(), "undefined", marketDataRequestsKey(marketDataRequest), marketDataRequestsByPopularityKey(),
+//       marketDataRequest.marketDataType, marketDataRequest, marketDataRequest, marketDataRequest, marketDataRequest,
+//       0
+//     ]
+//   },
+//   transformReply() {}
+// })
 
 // runs when the client has connected to the redis instance:
 redisClient.on("connect", async () => {
