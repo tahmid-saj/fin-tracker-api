aeadea81b639aeea124959e7584d6e6688cc463a
tahmid-saj <tahmid.saj@gmail.com>
2025-06-03 20:08:33 -0400

    caching chatbot session

--- src/routes/chatbot/chatbot.controller.ts
+++ src/routes/chatbot/chatbot.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express"
 import { getChatBotResponse, getChatBotResponseStream } from "../../utils/requests/chatbot/chatbot.requests.js"
 import { RANDOM_SEPARATOR } from "../../utils/constants/chatbot.constants.js"
+import { getChatbotSessionRequests, saveChatbotSessionRequest, 
+  sessionIdExists } from "../../redis/queries/chatbot/chatbot.queries.js"
 
 // chatbot response
 async function httpGetChatBotResponse(req: Request, res: Response): Promise<void> {
@@ -27,15 +29,34 @@ async function httpGetChatBotResponseStream(req: Request, res: Response) {
 
   const messageInput = String(req.body)
 
+  // const response: string[] = []
   await getChatBotResponseStream(messageInput, (chunk: string) => {
+    // response.push(chunk)
     res.write(`data: ${chunk}${RANDOM_SEPARATOR}`)
   })
 
+  // const sessionId = await saveChatbotSessionRequest(messageInput, response.join(""))
+
   res.write(`data: [DONE]${RANDOM_SEPARATOR}`)
-  res.end()
+  // res.end(sessionId)
+}
+
+// get chatbot session (requests and responses)
+async function httpGetChatBotSession(req: Request, res: Response) {
+  const sessionId = String(req.body)
+
+  // if the session exists, then return the session's requests and responses
+  const sessionExists = await sessionIdExists(sessionId)
+  if (sessionExists) {
+    const sessionData = await getChatbotSessionRequests(sessionId)
+    if (sessionData) {
+      res.status(200).json(sessionData)
+    }
+  }
 }
 
 export {
   httpGetChatBotResponse,
-  httpGetChatBotResponseStream
+  httpGetChatBotResponseStream,
+  httpGetChatBotSession
 }
