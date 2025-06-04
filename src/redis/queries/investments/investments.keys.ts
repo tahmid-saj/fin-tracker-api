48cebbf13ef9d5050a9b717429afc44474138529
tahmid-saj <tahmid.saj@gmail.com>
2025-06-04 18:20:04 -0400

    caching investments

--- src/redis/queries/investments/investments.keys.ts
+++ src/redis/queries/investments/investments.keys.ts
@@ -1,12 +1,12 @@
-import { User } from "../../../models/users/users.types";
+import { User } from "../../../models/users/users.types.js";
 
 // set containing all of investment names of user:
-export const userInvestmentsKey = (user: User) => `investments#${user.userId}:${user.email}`
+export const userInvestmentsKey = (user: User) => `user-investments#${user.userId}:${user.email}`
 
 // hash containing investment fields
-export const investmentKey = (user: User, investmentName: string) => `investments#${user.userId}:${user.email}:${investmentName}`
+export const investmentKey = (user: User, investmentName: string) => `user-investment#${user.userId}:${user.email}:${investmentName}`
 
 // list containing investment calculation records
-export const investmentCalculationRecordsKey = (user: User, investmentName: string) => `investments:calc-record#${user.userId}:${user.email}:${investmentName}`
+export const investmentCalculationRecordsKey = (user: User, investmentName: string) => `investment:calc-records#${user.userId}:${user.email}:${investmentName}`
 
 export const investmentsSummaryKey = (user: User) => `investments-summary#${user.userId}:${user.email}`
--- src/redis/queries/investments/investments.queries.ts
+++ src/redis/queries/investments/investments.queries.ts
@@ -1,31 +1,93 @@
-import { Investment, InvestmentCalculationRecord, InvestmentsSummary } from "../../../models/investments/investments.types";
-import { User } from "../../../models/users/users.types";
-import { redisClient } from "../../../services/redis/redis.service";
-import { investmentCalculationRecordsKey, investmentKey, investmentsSummaryKey, userInvestmentsKey } from "./investments.keys";
+import { Investment, InvestmentCalculationRecord, 
+  InvestmentsSummary } from "../../../models/investments/investments.types.js";
+import { User } from "../../../models/users/users.types.js";
+import { redisClient } from "../../../services/redis/redis.service.js";
+import { investmentCalculationRecordsKey, investmentKey, 
+  investmentsSummaryKey, userInvestmentsKey } from "./investments.keys.js";
 
 // helper functions
 export const serializeInvestment = (investment: Investment) => {
-  return {}
+  return {
+    investmentName: investment.investmentName,
+    investmentType: investment.investmentType,
+    startingAmount: investment.startingAmount,
+    startDate: String(investment.startDate),
+    afterYears: investment.afterYears,
+    returnRate: investment.returnRate,
+    compounded: investment.compounded,
+    additionalContribution: investment.additionalContribution,
+    contributionAt: investment.contributionAt,
+    contributionInterval: investment.contributionInterval,
+
+    endBalance: investment.endBalance,
+    totalContribution: investment.totalContribution,
+    totalInterest: investment.totalInterest
+  }
 }
 
-export const serializeInvestmentCalculationRecords = (investment: Investment) => {
-  return []
+export const serializeInvestmentCalculationRecords = (investments: InvestmentCalculationRecord[]) => {
+  return investments.map((investmentRecord) => {
+    return `date=${investmentRecord.currentDate}!contrib=${investmentRecord.contribution}!interestAcc=${investmentRecord.interestAccumulated}!endBalance=${investmentRecord.endingBalance}`
+  })
 }
 
 export const serializeInvestmentsSummary = (investmentsSummary: InvestmentsSummary) => {
-  return {}
+  let summary: InvestmentsSummary = {
+    currentAllInvestmentsBalance: 0,
+    totalAllContribution: 0,
+    totalAllInterest: 0
+  }
+
+  if (investmentsSummary.currentAllInvestmentsBalance) summary.currentAllInvestmentsBalance = investmentsSummary.currentAllInvestmentsBalance
+  if (investmentsSummary.totalAllContribution) summary.totalAllContribution = investmentsSummary.totalAllContribution
+  if (investmentsSummary.totalAllInterest) summary.totalAllInterest = investmentsSummary.totalAllInterest
+
+  return summary
 }
 
-export const deserializeInvestment = (investment: { [key: string]: string }) => {
-  return {}
+export const deserializeInvestment = (investment: { [key: string]: string }): Investment => {  
+  return {
+    investmentName: investment.investmentName!,
+    investmentType: investment.investmentType!,
+    startingAmount: Number(investment.startingAmount)!,
+    startDate: investment.startDate!,
+    afterYears: Number(investment.afterYears!),
+    returnRate: Number(investment.returnRate!),
+    compounded: investment.compounded!,
+    additionalContribution: Number(investment.additionalContribution!),
+    contributionAt: investment.contributionAt!,
+    contributionInterval: investment.contributionInterval!,
+
+    endBalance: Number(investment.endBalance!),
+    totalContribution: Number(investment.totalContribution!),
+    totalInterest: Number(investment.totalInterest!),
+
+    investments: []
+  }
 }
 
-export const deserializeInvestmentCalculationRecords = (investmentCalculationRecords: string[]) => {
-  return []
+export const deserializeInvestmentCalculationRecords = (investmentCalculationRecords: string[]): InvestmentCalculationRecord[] => {
+  return investmentCalculationRecords.map((record) => {
+    const data = record.split("!")
+    const currentDate = data[0]?.split("=")[1]!
+    const contribution = Number(data[1]?.split("=")[1])
+    const interestAccumulated = Number(data[2]?.split("=")[1])
+    const endingBalance = Number(data[3]?.split("=")[1])
+
+    return {
+      currentDate, contribution, interestAccumulated, endingBalance
+    }
+  })
 }
 
 export const deserializeInvestmentsSummary = (investmentsSummary: { [key: string]: string }) => {
-  return {}
+  let summary: InvestmentsSummary = {}
+  if (investmentsSummary.currentAllInvestmentsBalance) summary.currentAllInvestmentsBalance = Number(investmentsSummary.currentAllInvestmentsBalance)
+  if (investmentsSummary.totalAllContribution) summary.totalAllContribution = Number(investmentsSummary.totalAllContribution)
+  if (investmentsSummary.totalAllInterest) summary.totalAllInterest = Number(investmentsSummary.totalAllInterest)
+  return {
+    investmentsSummary: summary
+  }
 }
 
 export const areInvestmentsCached = async (user: User) => {
@@ -38,41 +100,44 @@ export const isInvestmentsSummaryCached = async (user: User) => {
 
 export const getInvestments = async (user: User) => {
   const investments = await redisClient.sMembers(userInvestmentsKey(user))
-  
-  return {
-    investments: investments.map(async (investment) => {
+
+  const resInvestments = await Promise.all(
+    investments.map(async (investment) => {
       const resInvestment = await redisClient.hGetAll(investmentKey(user, investment))
       const resInvestmentCalculationRecords = await redisClient.lRange(investmentCalculationRecordsKey(user, investment), 0, -1)
 
       const deserializedInvestment = deserializeInvestment(resInvestment)
-      const deserializedInvestmentCalculationRecords = deserializeInvestmentCalculationRecords(resInvestmentCalculationRecords)
+      deserializedInvestment.investments = deserializeInvestmentCalculationRecords(resInvestmentCalculationRecords)
 
-      return {
-        ...deserializedInvestment,
-        investments: deserializedInvestmentCalculationRecords
-      }
+      return deserializedInvestment
     })
+  )
+
+  return {
+    investments: resInvestments
   }
 }
 
 export const getInvestmentsSummary = async (user: User) => {
   const investmentsSummary = await redisClient.hGetAll(investmentsSummaryKey(user))
-  return deserializeInvestment(investmentsSummary)
+  return deserializeInvestmentsSummary(investmentsSummary)
 }
 
 export const saveInvestments = async (user: User, investments: Investment[]) => {
   investments.map(async (investment) => {
+    const serializedInvestment = serializeInvestment(investment)
+    const serializedInvestmentCalculationRecords = serializeInvestmentCalculationRecords(investment.investments)
+
     await Promise.all([
       // add the investment to the investments set
       redisClient.sAdd(userInvestmentsKey(user), investment.investmentName),
 
       // add the investment fields to the hash
-      redisClient.hSet(investmentKey(user, investment.investmentName), 
-        serializeInvestment(investment)),
+      redisClient.hSet(investmentKey(user, investment.investmentName),serializedInvestment),
 
       // add the investment calculation records to the list
       redisClient.rPush(investmentCalculationRecordsKey(user, investment.investmentName),
-        serializeInvestmentCalculationRecords(investment))
+        serializedInvestmentCalculationRecords)
     ])
   })
 }
